7d51144c494c47325cb306130c09d4929ba53ee1
IAndrewJJ <100313205+IAndrewJJ@users.noreply.github.com>
2022-02-28 15:19:11 -0700

    Update StefanBrokenBash.sh

--- StefanBrokenBash.sh
+++ StefanBrokenBash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#changed y to b in line 8 to account for 2nd variable b.
+#changed y to b in line 8 to account for 2nd variable b.#
 echo "Enter number"
 read a
 echo "Enter second number"
