f213ea1d09bd7165f64b4608389bb95a68d0db0e
skosanovic <100165904+skosanovic@users.noreply.github.com>
2022-02-28 14:41:46 -0700

    Add files via upload

---
+++ StefanBrokenBash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Enter number"
+read a
+echo "Enter second number"
+read b
+(( sum=a+y ))
+echo "The total is $sum"
---
+++ StefanBrokenPython.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+#program to convert kilometers to miles
+kilometers = float(input("Enter amount of kilometers: "))
+conversion = 0.621371
+miles = kilometers * con_factor
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles)
+
