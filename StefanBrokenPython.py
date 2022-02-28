fc3f255c0f18ebc4ee2cb690cbe9751e312afb5f
IAndrewJJ <100313205+IAndrewJJ@users.noreply.github.com>
2022-02-28 15:13:02 -0700

    Update StefanBrokenPython.py

--- StefanBrokenPython.py
+++ StefanBrokenPython.py
@@ -2,6 +2,6 @@
 #program to convert kilometers to miles
 kilometers = float(input("Enter amount of kilometers: "))
 conversion = 0.621371
-miles = kilometers * con_factor
-print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles)
+miles = kilometers * conversion
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
 
